b61c6da462d6f4e2ff1976c9c6ef4983efd5198c
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-08 11:08:14 +0900

    first commit

---
+++ src/main/java/com/ryohaseu/to_do_list_with_dto/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.ryohaseu.to_do_list_with_dto.controller;
+
+
+import com.ryohaseu.to_do_list_with_dto.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+    private final UserRepository userRepository;
+
+    @GetMapping({"/", "/login"})
+    public String showLogin() {
+        return "login";
+    }
+}
